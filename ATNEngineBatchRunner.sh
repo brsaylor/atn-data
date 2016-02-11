5f7a7bb38f825780f3d71fbb2a9c5faa1ab2401f
Ben Saylor <brsaylor@gmail.com>
2016-02-11 10:32:37 -0800

    Add all Convergence game ecosystems

--- ATNEngineBatchRunner.sh
+++ ATNEngineBatchRunner.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+# Args: <timesteps> <node config input file>
+
 cd ../WoB_Server_ATNEngine
 java -cp 'build/libs/WoB_Server_ATNEngine.jar:lib/*:chartlib/*' atn.ATNEngineBatchRunner $1 ../atn-data/$2
