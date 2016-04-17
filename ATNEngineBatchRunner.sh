1049b1b22afe45f3d1c26106729b2787e1fcfadf
Ben Saylor <brsaylor@gmail.com>
2016-04-17 14:21:57 -0700

    Add --use-webservices argument to ATNBatchRunner.sh

--- ATNEngineBatchRunner.sh
+++ ATNEngineBatchRunner.sh
@@ -3,4 +3,4 @@
 # Args: <timesteps> <node config input file>
 
 cd ../WoB_Server_ATNEngine
-java -cp 'build/libs/WoB_Server_ATNEngine.jar:lib/*:chartlib/*' atn.ATNEngineBatchRunner $1 ../atn-data/$2
+java -cp 'build/libs/WoB_Server_ATNEngine.jar:lib/*:chartlib/*' atn.ATNEngineBatchRunner $1 ../atn-data/$2 $3
