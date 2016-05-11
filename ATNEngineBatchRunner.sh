cee354d31b7452b2d4bd47a061e7263eee4d16f0
Ben Saylor <brsaylor@gmail.com>
2016-05-11 11:29:34 -0700

    ATNEngineBatchRunner.sh: optionally enable assertions

--- ATNEngineBatchRunner.sh
+++ ATNEngineBatchRunner.sh
@@ -2,5 +2,10 @@
 
 # Args: <timesteps> <node config input file>
 
+# Uncomment to enable assertions
+#JVM_ARGS="-enableassertions"
+
 cd ../WoB_Server_ATNEngine
-java -cp 'build/libs/WoB_Server_ATNEngine.jar:lib/*:chartlib/*' atn.ATNEngineBatchRunner $1 ../atn-data/$2 $3
+java $JVM_ARGS \
+    -cp 'build/libs/WoB_Server_ATNEngine.jar:lib/*:chartlib/*' \
+    atn.ATNEngineBatchRunner $1 ../atn-data/$2 $3
