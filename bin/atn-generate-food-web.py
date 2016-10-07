f1515eaaff3975e0020af6cf56f423b81a207762
Ben Saylor <brsaylor@gmail.com>
2016-10-06 21:37:37 -0700

    atn-generate-food-web: fix path handling

--- bin/atn-generate-food-web.py
+++ bin/atn-generate-food-web.py
@@ -13,7 +13,7 @@ if args.existing_dir is None:
     print("--existing-dir is required for now", file=sys.stderr)
     sys.exit(1)
 
-food_web_id = os.path.normpath(args.existing_dir)
+food_web_id = os.path.basename(os.path.normpath(args.existing_dir))
 
 node_ids = [int(x) for x in food_web_id.split('-')]
 
@@ -21,7 +21,7 @@ serengeti = foodwebs.read_serengeti()
 subweb = serengeti.subgraph(node_ids)
 
 foodwebs.draw_food_web(subweb, show_legend=True,
-                       output_file=os.path.join(food_web_id, 'foodweb.{}.png'.format(food_web_id)))
+    output_file=os.path.join(args.existing_dir, 'foodweb.{}.png'.format(food_web_id)))
 
-with open(os.path.join(food_web_id, 'foodweb.{}.json'.format(food_web_id)), 'w') as f:
+with open(os.path.join(args.existing_dir, 'foodweb.{}.json'.format(food_web_id)), 'w') as f:
     print(foodwebs.food_web_json(subweb), file=f)
