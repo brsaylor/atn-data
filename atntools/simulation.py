31335a12f8cc706534063fea68213fd7cd8fcc40
Ben Saylor <brsaylor@gmail.com>
2016-11-25 21:36:45 -0800

    Implement atn-engine-batch-runner.py

---
+++ atntools/simulation.py
@@ -0,0 +1,26 @@
+"""
+Functions for running simulations
+"""
+
+import os
+import subprocess
+
+from atntools import settings
+
+
+def atn_engine_batch_runner(timesteps, node_config_file, use_webservices=False, use_csv=False, output_dir=None):
+    """ Run ATNEngineBatchRunner from WoB Server with the given arguments. """
+
+    node_config_file = os.path.abspath(os.path.expanduser(node_config_file))
+    args = ['java', '-cp', 'build/libs/WoB_Server_ATNEngine.jar:lib/*:chartlib/*',
+               'atn.ATNEngineBatchRunner', str(timesteps), node_config_file]
+    if use_webservices:
+        args.append('--use-webservices')
+    if use_csv:
+        args.append('--use-csv')
+    if output_dir:
+        output_dir = os.path.abspath(os.path.expanduser(output_dir))
+        args.extend(['--output-dir', output_dir])
+    args.append('blah')
+    process = subprocess.run(args, cwd=settings.WOB_SERVER_HOME)
+    process.check_returncode()
---
+++ bin/atn-engine-batch-runner.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+""" Executes ATNEngineBatchRunner from WoB Server to run a batch of simulations. """
+
+import argparse
+
+from atntools.simulation import atn_engine_batch_runner
+
+
+parser = argparse.ArgumentParser(description=globals()['__doc__'])
+parser.add_argument('timesteps', type=int,
+                    help="Number of timesteps to run all simulations")
+parser.add_argument('node_config_file',
+                    help="File containing node config strings, one per line")
+parser.add_argument('--use-webservices', action='store_true',
+                    help="Use Web Services to run the simulation, rather than running locally")
+parser.add_argument('--use-csv', action='store_true',
+                    help="Save output data in CSV format instead of HDF5 format")
+parser.add_argument('--output-dir', help="Output directory (default: $WOB_SERVER_HOME/src/log/(atn|sim)")
+args = parser.parse_args()
+
+atn_engine_batch_runner(**vars(args))
