d97e57613af7664a4be6cc6f7c95097ad0af0cb3
Ben Saylor <brsaylor@gmail.com>
2017-02-02 13:32:46 -0800

    Add from-node-ids option to atn-generate-food-web.py

--- bin/atn-generate-food-web.py
+++ bin/atn-generate-food-web.py
@@ -17,17 +17,21 @@ from atntools import foodwebs
 from atntools import util
 
 parser = argparse.ArgumentParser(description=globals()['__doc__'])
+parser.add_argument('--parent-dir', help="Parent directory to use instead of automatically-determined directory under DATA_HOME")
 subparsers = parser.add_subparsers(dest='subparser_name')
 
 # 'generate' sub-command
 parser_generate = subparsers.add_parser('generate', help="Generate a new food web and save plot and JSON")
 parser_generate.add_argument('size', type=int, help="Number of species")
 parser_generate.add_argument('num_basal_species', type=int, help="Number of basal species")
-parser_generate.add_argument('--parent-dir', help="Parent directory")
 
 # 'regenerate' sub-command
 parser_regenerate = subparsers.add_parser('regenerate', help="Regenerate files in existing food web directory")
 parser_regenerate.add_argument('existing_dir', help="Existing food web directory")
+
+# 'from-node-ids' sub-command
+parser_from_node_ids = subparsers.add_parser('from-node-ids', help="Generate plot and JSON from given node IDs")
+parser_from_node_ids.add_argument('node_ids', nargs='+', type=int, help="List of node IDs")
 
 args = parser.parse_args()
 
@@ -58,6 +62,19 @@ elif args.subparser_name == 'regenerate':
     node_ids = [int(x) for x in food_web_id.split('-')]
     serengeti = foodwebs.read_serengeti()
     subweb = serengeti.subgraph(node_ids)
+
+elif args.subparser_name == 'from-node-ids':
+
+    node_ids = sorted(args.node_ids)
+    serengeti = foodwebs.read_serengeti()
+    subweb = serengeti.subgraph(node_ids)
+    food_web_id = '-'.join([str(x) for x in node_ids])
+    if args.parent_dir is None:
+        food_web_dir = util.get_food_web_dir(food_web_id)
+    else:
+        food_web_dir = os.path.join(os.path.expanduser(args.parent_dir), food_web_id)
+    print("Creating food web directory " + food_web_dir)
+    os.makedirs(food_web_dir)
 
 foodwebs.draw_food_web(subweb, show_legend=True,
                        output_file=os.path.join(food_web_dir, 'foodweb.{}.png'.format(food_web_id)))
