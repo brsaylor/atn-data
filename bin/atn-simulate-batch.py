47c3d32616f9c78728dca9228d962cf8cde451a1
Ben Saylor <brsaylor@gmail.com>
2017-05-08 17:31:34 -0700

    Allow passing --no-record-biomass, --no-stop-on-steady-state to atn-simulate-batch.py

--- bin/atn-simulate-batch.py
+++ bin/atn-simulate-batch.py
@@ -3,12 +3,19 @@
 """ Runs a batch of simulations for a given set. """
 
 import argparse
+import copy
 
 from atntools import simulation
 
 parser = argparse.ArgumentParser(description=globals()['__doc__'])
 parser.add_argument('set_number', type=int, help="Set number (set directory must exist under DATA_HOME)")
 parser.add_argument('timesteps', type=int, help="Number of time steps to run the simulations")
+parser.add_argument('--no-record-biomass', action='store_true')
+parser.add_argument('--no-stop-on-steady-state', action='store_true')
 args = parser.parse_args()
 
-simulation.simulate_batch(args.set_number, args.timesteps)
+kwargs = copy.copy(vars(args))
+del kwargs['set_number']
+del kwargs['timesteps']
+
+simulation.simulate_batch(args.set_number, args.timesteps, **kwargs)
